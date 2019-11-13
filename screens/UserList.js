3d401413ca231bd6eb21f8643c0cf6bfa773db2e
webmaster <webmasterdragon@outlook.com>
2019-11-13 21:56:48 +0700

    styled component init

--- screens/UserList.js
+++ screens/UserList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View, Button } from 'react-native';
 import Unsplash from 'unsplash-js';
+import styled from 'styled-components';
 
 const unsplash = new Unsplash({
   accessKey: 'aa2f3c3be8125f1fc86e3007153420c4e446c19b7b0c6d80a6257b281c9a0dc5'
 });
+
+const Container = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default class UserListScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -21,12 +30,12 @@ export default class UserListScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Container>
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('User')}
         />
-      </View>
+      </Container>
     );
   }
 }
